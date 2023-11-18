d6a22fa3d657ab3ffe5a1d6822f2a2874d81da89
Andreas <116385001+plaidraid@users.noreply.github.com>
2023-11-17 16:49:05 -0800

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -27,84 +27,11 @@ $('#navigation').append('' +
                 '</li>' +
             '</ul>' +
         '</li>' +
-       '<li class="pcoded-hasmenu">' +
-           '<a href="javascript:void(0)">' +
-               '<span class="pcoded-micon"><i class="feather icon-sidebar"></i></span>' +
-               '<span class="pcoded-mtext">Page layouts</span>' +
-               '<span class="pcoded-badge label label-warning">NEW</span>' +
+       '<li class="">' +
+           '<a href="https://github.com/users/plaidraid/projects/4/views/1">' +
+               '<span class="pcoded-micon"><i class="feather icon-menu"></i></span>' +
+               '<span class="pcoded-mtext">Projects</span>' +
            '</a>' +
-           '<ul class="pcoded-submenu">' +
-               '<li class=" pcoded-hasmenu">' +
-                   '<a href="javascript:void(0)">' +
-                       '<span class="pcoded-mtext">Vertical</span>' +
-                   '</a>' +
-                   '<ul class="pcoded-submenu">' +
-                       '<li class=" ">' +
-                           '<a href="menu-static.htm">' +
-                               '<span class="pcoded-mtext">Static Layout</span>' +
-                           '</a>' +
-                       '</li>' +
-                       '<li class=" ">' +
-                           '<a href="menu-header-fixed.htm">' +
-                               '<span class="pcoded-mtext">Header Fixed</span>' +
-                           '</a>' +
-                       '</li>' +
-                       '<li class=" ">' +
-                           '<a href="menu-compact.htm">' +
-                               '<span class="pcoded-mtext">Compact</span>' +
-                           '</a>' +
-                       '</li>' +
-                       '<li class=" ">' +
-                           '<a href="menu-sidebar.htm">' +
-                               '<span class="pcoded-mtext">Sidebar Fixed</span>' +
-                           '</a>' +
-                       '</li>' +
-
-                   '</ul>' +
-               '</li>' +
-               '<li class=" pcoded-hasmenu">' +
-                   '<a href="javascript:void(0)">' +
-                       '<span class="pcoded-mtext">Horizontal</span>' +
-                   '</a>' +
-                   '<ul class="pcoded-submenu">' +
-                       '<li class=" ">' +
-                           '<a href="menu-horizontal-static.htm" target="_blank">' +
-                               '<span class="pcoded-mtext">Static Layout</span>' +
-                           '</a>' +
-                       '</li>' +
-                       '<li class=" ">' +
-                           '<a href="menu-horizontal-fixed.htm" target="_blank">' +
-                               '<span class="pcoded-mtext">Fixed layout</span>' +
-                           '</a>' +
-                       '</li>' +
-                       '<li class=" ">' +
-                           '<a href="menu-horizontal-icon.htm" target="_blank">' +
-                               '<span class="pcoded-mtext">Static With Icon</span>' +
-                           '</a>' +
-                       '</li>' +
-                       '<li class=" ">' +
-                           '<a href="menu-horizontal-icon-fixed.htm" target="_blank">' +
-                               '<span class="pcoded-mtext">Fixed With Icon</span>' +
-                           '</a>' +
-                       '</li>' +
-                   '</ul>' +
-               '</li>' +
-               '<li class=" ">' +
-                   '<a href="menu-bottom.htm">' +
-                       '<span class="pcoded-mtext">Bottom Menu</span>' +
-                   '</a>' +
-               '</li>' +
-               '<li class=" ">' +
-                   '<a href="box-layout.htm" target="_blank">' +
-                       '<span class="pcoded-mtext">Box Layout</span>' +
-                   '</a>' +
-               '</li>' +
-               '<li class=" ">' +
-                   '<a href="menu-rtl.htm" target="_blank">' +
-                       '<span class="pcoded-mtext">RTL</span>' +
-                   '</a>' +
-               '</li>' +
-           '</ul>' +
        '</li>' +
        '<li class="">' +
            '<a href="navbar-light.htm">' +
