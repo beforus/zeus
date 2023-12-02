d2d236268d50e81bee49d4f9e483e7899a298f57
Andreas <116385001+foremily@users.noreply.github.com>
2023-12-02 04:57:05 -0800

    Update script.js

--- includes/assets/js/script.js
+++ includes/assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     //add id to main menu for mobile menu end
 
     $.get("/e/navigation.htm", function(data){
-    $('#navigator').innerHTML = data;
+    $('.pcoded-navbar').innerHTML = data;
     });
 
     // card js start
